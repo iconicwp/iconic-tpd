d8f2bdc6dccd4966b6bed21194cf5af0094d6009
James Kemp <james@madebyplatform.com>
2021-02-27 08:58:14 +0000

    Allow in admin too

--- iconic-tpd.php
+++ iconic-tpd.php
@@ -228,5 +228,5 @@ function iconic_tpd_is_enabled() {
 		include ABSPATH . 'wp-includes/pluggable.php';
 	}
 
-	return current_user_can( 'administrator' ) && ! is_admin();
+	return current_user_can( 'administrator' ) && strpos( $_SERVER['REQUEST_URI'], '/wp-admin/plugins.php' ) !== 0;
 }
